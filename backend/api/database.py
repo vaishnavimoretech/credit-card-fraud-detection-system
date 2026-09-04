fef3c286edeac4079f7fc81e7c7483dd7ac7ce27
Vaishnavi <vaishnavimore4889@gmail.com>
2026-09-04 21:30:26 +0530

    remove hardcoded database credentials

--- backend/api/database.py
+++ backend/api/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:prachi09@localhost/fraud_detection"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
@@ -12,6 +16,7 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
 def get_db():
     db = SessionLocal()
     try:
