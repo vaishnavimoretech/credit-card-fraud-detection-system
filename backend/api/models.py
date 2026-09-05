3140e0a29827d11ecaf48fa807a997daae166cf9
Vaishnavi <vaishnavimore4889@gmail.com>
2026-09-06 01:05:52 +0530

    Fix User model password column

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,4 +7,4 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
