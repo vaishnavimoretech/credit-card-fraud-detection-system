cf4f5c98bac354fcf8111be95856639f677937ae
Vaishnavi <vaishnavimore4889@gmail.com>
2026-09-05 23:34:07 +0530

    Fix CORS for deployed frontend

--- backend/api/main.py
+++ backend/api/main.py
@@ -19,9 +19,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000"
+   allow_origins=[
+        "https://credit-card-fraud-detection-system-livid.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
