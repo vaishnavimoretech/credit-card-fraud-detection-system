1dfaa0a69aca6ab180526b99c4427e0f54197409
Vaishnavi <vaishnavimore4889@gmail.com>
2026-09-06 00:38:39 +0530

    Fix CORS configuration

--- backend/api/main.py
+++ backend/api/main.py
@@ -15,17 +15,20 @@ import pandas as pd
 import joblib
 import numpy as np
 
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-   allow_origins=[
+    allow_origins=[
         "https://credit-card-fraud-detection-system-livid.vercel.app"
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-) 
+)
 
 app.include_router(auth_router)
 
